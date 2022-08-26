2e8c08c976f523d7d43ab6f5d94ef4dcaf5c2901
zhaoyuntao <zhaoyuntao888@gmail.com>
2022-08-26 13:55:18 +0400

    0826

--- src/test/A.java
+++ src/test/A.java
@@ -1,17 +1,27 @@
 package test;
 
+import java.io.*;
+import java.nio.CharBuffer;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 
 public class A {
     public static void main(String[] args) {
-        Map<String,String> map1=new HashMap<>();
-        Map<String,String> map2=new HashMap<>();
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(new File("abc.txt"), "rw");
+             RandomAccessFile reader = new RandomAccessFile(new File("abc.txt"),"r");
+        ) {
+            String abc = "abcde";
+            randomAccessFile.seek(1);
+            randomAccessFile.write(abc.getBytes(),0,3);
+            String a;
+            reader.seek(2);
+            while ((a = reader.readLine()) != null) {
+                S.s(a);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-        map1.put("map1","map1");
-        map2.put("map2","map2");
-        map2.putAll(map1);
-        S.s(map1);
-        S.s(map2);
     }
 }
